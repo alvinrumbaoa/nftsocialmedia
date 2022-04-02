f43a5609ecfb196040018a3027c561c1d8b80c1c
Alvin Rumbaoa <alvinrumbaoa@gmail.com>
2022-04-02 00:55:02 -0700

    s

--- server/index.js
+++ server/index.js
@@ -3,7 +3,10 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv'
 
+
+import userRouter from './routes/user.routes.js';
 import postRoutes from './routes/posts.routes.js';
 
 const app = express();
@@ -12,11 +15,13 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-app.use('/posts', postRoutes);
+dotenv.config();
 
-const CONNECTION_URL  ='mongodb+srv://xenodochy23:Senpou_23@cluster0.f6bs6.mongodb.net/nftcollection?retryWrites=true&w=majority'
+app.use('/posts', postRoutes);
+app.use("/user", userRouter);
+
 const PORT = process.env.PORT|| 8000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
